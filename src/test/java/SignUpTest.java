1763e843f4a3e3caa4c4bc241154568cf0b03d28
NoQuea <pasha_vorobyov@inbox.ru>
2021-11-18 23:27:44 +0300

    mistakes fixed, update code

--- src/test/java/SignUpTest.java
+++ src/test/java/SignUpTest.java
@@ -9,7 +9,7 @@ import org.testng.annotations.Test;
 
 public class SignUpTest {
 
-    WebDriver driver = null;
+    WebDriver driver;
 
     @BeforeMethod
     public void enterBrowser() {
@@ -20,159 +20,165 @@ public class SignUpTest {
 
     @Test
     public void validValues() {
-        WebElement zipCodeFiveDigits = driver.findElement(By.xpath("//input[@name=\"zip_code\"]"));
-        zipCodeFiveDigits.sendKeys("12345");
+        driver.findElement(By.xpath("//input[@name=\"zip_code\"]"))
+                .sendKeys("12345");
         WebElement clickOnTheButtonContinue = driver.findElement(By.xpath("//input[@value=\"Continue\"]"));
         clickOnTheButtonContinue.click();
 
         //Ввод валидных значений в поля со звездочками
-        WebElement firstName = driver.findElement(By.xpath("//input[@name=\"first_name\"]"));
-        firstName.sendKeys("Pavel");
-        WebElement email = driver.findElement(By.xpath("//input[@name=\"email\"]"));
-        email.sendKeys("sdfonlasdbg@mail.ru");
-        WebElement password1 = driver.findElement(By.xpath("//input[@name=\"password1\"]"));
-        password1.sendKeys("111111");
-        WebElement password2 = driver.findElement(By.xpath("//input[@name=\"password2\"]"));
-        password2.sendKeys("111111");
+        driver.findElement(By.xpath("//input[@name=\"first_name\"]"))
+                .sendKeys("Pavel");
+        driver.findElement(By.xpath("//input[@name=\"email\"]"))
+                .sendKeys("sdfonlasdbg@mail.ru");
+        driver.findElement(By.xpath("//input[@name=\"password1\"]"))
+                .sendKeys("111111");
+        driver.findElement(By.xpath("//input[@name=\"password2\"]"))
+                .sendKeys("111111");
 
         //Нажимаем на конпку Register
-        WebElement clickOnbButtonRegister = driver.findElement(By.cssSelector("[value=Register]"));
-        clickOnbButtonRegister.click();
+        driver.findElement(By.cssSelector("[value=Register]"))
+                .click();
 
         //Убеждаемся что мы на странице окончания регистрации
-        boolean isEnd = driver.findElement(By.xpath("//span[@class=\"confirmation_message\"]")).isDisplayed();
+        boolean isEnd = driver.findElement(By.xpath("//span[@class=\"confirmation_message\"]"))
+                .isDisplayed();
         Assert.assertTrue(isEnd);
 
     }
 
     @Test
     public void withoutFirstName() {
-        WebElement zipCodeFiveDigits = driver.findElement(By.xpath("//input[@name=\"zip_code\"]"));
-        zipCodeFiveDigits.sendKeys("12345");
-        WebElement clickOnTheButtonContinue = driver.findElement(By.xpath("//input[@value=\"Continue\"]"));
-        clickOnTheButtonContinue.click();
+        driver.findElement(By.xpath("//input[@name=\"zip_code\"]"))
+                .sendKeys("12345");
+        driver.findElement(By.xpath("//input[@value=\"Continue\"]"))
+                .click();
 
         //Ввод валидных значений в поля со звездочками не заполняя поле first name
-        WebElement email = driver.findElement(By.xpath("//input[@name=\"email\"]"));
-        email.sendKeys("sdfonlasdbg@mail.ru");
-        WebElement password1 = driver.findElement(By.xpath("//input[@name=\"password1\"]"));
-        password1.sendKeys("111111");
-        WebElement password2 = driver.findElement(By.xpath("//input[@name=\"password2\"]"));
-        password2.sendKeys("111111");
+        driver.findElement(By.xpath("//input[@name=\"email\"]"))
+                .sendKeys("sdfonlasdbg@mail.ru");
+        driver.findElement(By.xpath("//input[@name=\"password1\"]"))
+                .sendKeys("111111");
+        driver.findElement(By.xpath("//input[@name=\"password2\"]"))
+                .sendKeys("111111");
 
         //Нажимаем на конпку Register
-        WebElement clickOnbButtonRegister = driver.findElement(By.cssSelector("[value=Register]"));
-        clickOnbButtonRegister.click();
+        driver.findElement(By.cssSelector("[value=Register]"))
+                .click();
 
         //Убеждаемся что нам выдало ошибку
-        boolean isEnd = driver.findElement(By.xpath("//span[@class=\"error_message\"]")).isDisplayed();
+        boolean isEnd = driver.findElement(By.xpath("//span[@class=\"error_message\"]"))
+                .isDisplayed();
         Assert.assertTrue(isEnd);
 
     }
 
     @Test
-    public void xml() {
-        WebElement zipCodeFiveDigits = driver.findElement(By.xpath("//input[@name=\"zip_code\"]"));
-        zipCodeFiveDigits.sendKeys("12345");
-        WebElement clickOnTheButtonContinue = driver.findElement(By.xpath("//input[@value=\"Continue\"]"));
-        clickOnTheButtonContinue.click();
+    public void fillXmlTagInFirstNameFieldShouldDisplayErrorMessage() {
+        driver.findElement(By.xpath("//input[@name=\"zip_code\"]"))
+                .sendKeys("12345");
+        driver.findElement(By.xpath("//input[@value=\"Continue\"]"))
+                .click();
 
         //Ввод валидных значений в поля со звездочками заполняя поле first name XML запросом
-        WebElement firstName = driver.findElement(By.xpath("//input[@name=\"first_name\"]"));
-        firstName.sendKeys("<td><span class=\"user\"></span></td>");
-        WebElement email = driver.findElement(By.xpath("//input[@name=\"email\"]"));
-        email.sendKeys("sdfonlasdbg@mail.ru");
-        WebElement password1 = driver.findElement(By.xpath("//input[@name=\"password1\"]"));
-        password1.sendKeys("111111");
-        WebElement password2 = driver.findElement(By.xpath("//input[@name=\"password2\"]"));
-        password2.sendKeys("111111");
+        driver.findElement(By.xpath("//input[@name=\"first_name\"]"))
+                .sendKeys("<td><span class=\"user\"></span></td>");
+        driver.findElement(By.xpath("//input[@name=\"email\"]"))
+                .sendKeys("sdfonlasdbg@mail.ru");
+        driver.findElement(By.xpath("//input[@name=\"password1\"]"))
+                .sendKeys("111111");
+        driver.findElement(By.xpath("//input[@name=\"password2\"]"))
+                .sendKeys("111111");
 
         //Нажимаем на конпку Register
-        WebElement clickOnbButtonRegister = driver.findElement(By.cssSelector("[value=Register]"));
-        clickOnbButtonRegister.click();
+        driver.findElement(By.cssSelector("[value=Register]"))
+                .click();
 
         //Убеждаемся что нам выдало ошибку
-        boolean isEnd = driver.findElement(By.xpath("//span[@class=\"error_message\"]")).isDisplayed();
+        boolean isEnd = driver.findElement(By.xpath("//span[@class=\"error_message\"]"))
+                .isDisplayed();
         Assert.assertTrue(isEnd);
 
     }
 
     @Test
     public void xmlCapsLk() {
-        WebElement zipCodeFiveDigits = driver.findElement(By.xpath("//input[@name=\"zip_code\"]"));
-        zipCodeFiveDigits.sendKeys("12345");
-        WebElement clickOnTheButtonContinue = driver.findElement(By.xpath("//input[@value=\"Continue\"]"));
-        clickOnTheButtonContinue.click();
+        driver.findElement(By.xpath("//input[@name=\"zip_code\"]"))
+                .sendKeys("12345");
+        driver.findElement(By.xpath("//input[@value=\"Continue\"]"))
+                .click();
 
         //Ввод валидных значений в поля со звездочками заполняя поле first name XML запросом
-        WebElement firstName = driver.findElement(By.xpath("//input[@name=\"first_name\"]"));
-        firstName.sendKeys("<TD><SPAN CLASS=\"USER\"></SPAN></TD>");
-        WebElement email = driver.findElement(By.xpath("//input[@name=\"email\"]"));
-        email.sendKeys("sdfonlasdbg@mail.ru");
-        WebElement password1 = driver.findElement(By.xpath("//input[@name=\"password1\"]"));
-        password1.sendKeys("111111");
-        WebElement password2 = driver.findElement(By.xpath("//input[@name=\"password2\"]"));
-        password2.sendKeys("111111");
+        driver.findElement(By.xpath("//input[@name=\"first_name\"]"))
+                .sendKeys("<TD><SPAN CLASS=\"USER\"></SPAN></TD>");
+        driver.findElement(By.xpath("//input[@name=\"email\"]"))
+                .sendKeys("sdfonlasdbg@mail.ru");
+        driver.findElement(By.xpath("//input[@name=\"password1\"]"))
+                .sendKeys("111111");
+        driver.findElement(By.xpath("//input[@name=\"password2\"]"))
+                .sendKeys("111111");
 
         //Нажимаем на конпку Register
-        WebElement clickOnbButtonRegister = driver.findElement(By.cssSelector("[value=Register]"));
-        clickOnbButtonRegister.click();
+        driver.findElement(By.cssSelector("[value=Register]"))
+                .click();
 
         //Убеждаемся что нам выдало ошибку
-        boolean isEnd = driver.findElement(By.xpath("//span[@class=\"error_message\"]")).isDisplayed();
+        boolean isEnd = driver.findElement(By.xpath("//span[@class=\"error_message\"]"))
+                .isDisplayed();
         Assert.assertTrue(isEnd);
 
     }
 
     @Test
     public void confirmPassword() {
-        WebElement zipCodeFiveDigits = driver.findElement(By.xpath("//input[@name=\"zip_code\"]"));
-        zipCodeFiveDigits.sendKeys("12345");
-        WebElement clickOnTheButtonContinue = driver.findElement(By.xpath("//input[@value=\"Continue\"]"));
-        clickOnTheButtonContinue.click();
+        driver.findElement(By.xpath("//input[@name=\"zip_code\"]"))
+                .sendKeys("12345");
+        driver.findElement(By.xpath("//input[@value=\"Continue\"]"))
+                .click();
 
         //Ввод валидных значений в поля со звездочками заполняя поле Confirm Password отличное от Password
-        WebElement firstName = driver.findElement(By.xpath("//input[@name=\"first_name\"]"));
-        firstName.sendKeys("Pavel");
-        WebElement email = driver.findElement(By.xpath("//input[@name=\"email\"]"));
-        email.sendKeys("sdfonlasdbg@mail.ru");
-        WebElement password1 = driver.findElement(By.xpath("//input[@name=\"password1\"]"));
-        password1.sendKeys("111111");
-        WebElement password2 = driver.findElement(By.xpath("//input[@name=\"password2\"]"));
-        password2.sendKeys("222222");
+        driver.findElement(By.xpath("//input[@name=\"first_name\"]"))
+                .sendKeys("Pavel");
+        driver.findElement(By.xpath("//input[@name=\"email\"]"))
+                .sendKeys("sdfonlasdbg@mail.ru");
+        driver.findElement(By.xpath("//input[@name=\"password1\"]"))
+                .sendKeys("111111");
+        driver.findElement(By.xpath("//input[@name=\"password2\"]"))
+                .sendKeys("222222");
 
         //Нажимаем на конпку Register
-        WebElement clickOnbButtonRegister = driver.findElement(By.cssSelector("[value=Register]"));
-        clickOnbButtonRegister.click();
+        driver.findElement(By.cssSelector("[value=Register]"))
+                .click();
 
         //Убеждаемся что нам выдало ошибку
-        boolean isEnd = driver.findElement(By.xpath("//span[@class=\"error_message\"]")).isDisplayed();
+        boolean isEnd = driver.findElement(By.xpath("//span[@class=\"error_message\"]"))
+                .isDisplayed();
         Assert.assertTrue(isEnd, "We have a bug");
 
     }
     @Test
     public void empty() {
-        WebElement zipCodeFiveDigits = driver.findElement(By.xpath("//input[@name=\"zip_code\"]"));
-        zipCodeFiveDigits.sendKeys("12345");
-        WebElement clickOnTheButtonContinue = driver.findElement(By.xpath("//input[@value=\"Continue\"]"));
-        clickOnTheButtonContinue.click();
+        driver.findElement(By.xpath("//input[@name=\"zip_code\"]"))
+                .sendKeys("12345");
+        driver.findElement(By.xpath("//input[@value=\"Continue\"]"))
+                .click();
 
         //Ввод валидных значений в поля со звездочками заполняя поле Confirm Password отличное от Password
-        WebElement firstName = driver.findElement(By.xpath("//input[@name=\"first_name\"]"));
-        firstName.sendKeys("");
-        WebElement email = driver.findElement(By.xpath("//input[@name=\"email\"]"));
-        email.sendKeys("");
-        WebElement password1 = driver.findElement(By.xpath("//input[@name=\"password1\"]"));
-        password1.sendKeys("");
-        WebElement password2 = driver.findElement(By.xpath("//input[@name=\"password2\"]"));
-        password2.sendKeys("");
+        driver.findElement(By.xpath("//input[@name=\"first_name\"]"))
+                .sendKeys("");
+        driver.findElement(By.xpath("//input[@name=\"email\"]"))
+                .sendKeys("");
+        driver.findElement(By.xpath("//input[@name=\"password1\"]"))
+                .sendKeys("");
+        driver.findElement(By.xpath("//input[@name=\"password2\"]"))
+                .sendKeys("");
 
         //Нажимаем на конпку Register
-        WebElement clickOnbButtonRegister = driver.findElement(By.cssSelector("[value=Register]"));
-        clickOnbButtonRegister.click();
+        driver.findElement(By.cssSelector("[value=Register]"))
+                .click();
 
         //Убеждаемся что нам выдало ошибку
-        boolean isEnd = driver.findElement(By.xpath("//span[@class=\"error_message\"]")).isDisplayed();
+        boolean isEnd = driver.findElement(By.xpath("//span[@class=\"error_message\"]"))
+                .isDisplayed();
         Assert.assertTrue(isEnd);
 
     }
